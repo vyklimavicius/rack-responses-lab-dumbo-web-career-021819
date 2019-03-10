25ad12c872a5430ec6299227514eaa782b7f056a
Vytautas Klimavicius <vyklimavicius@gmail.com>
2019-03-10 20:18:10 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,11 @@
+def call(env)
+  resp = Rack::Response.new
+  time_now = Time.now.hour
+  if time_now < 12
+  resp.write "Good Morning"
+  else
+  resp.write "Good afternoon"
+  end
+  resp.finish
+end
+end
